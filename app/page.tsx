d6418b327a843c86b77bcb1bf908c8d052818cec
Rowleen <julianpeacevedo@gmail.com>
2025-11-27 00:53:19 +0100

    Create handle back and set step by product type

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,27 @@
 "use client";
 
 import { useState } from "react";
-import { Beer as BeerIcon, GlassWater, Martini, TestTube } from "lucide-react";
+import {
+  Beer as BeerIcon,
+  GlassWater,
+  Martini,
+  TestTube,
+  ChevronLeft,
+} from "lucide-react";
 
 import SectionProduct from "@/components/ui/SectionProduct/SectionProduct";
 import Card from "@/components/ui/Card/Card";
 
-import { ProductEnum, Product } from "@/entities/Product";
+import {
+  ProductEnum,
+  Product,
+  Drink,
+  Shot,
+  Soda,
+  Beer,
+  Other,
+  SubTypeProduct,
+} from "@/entities/Product";
 import { AzAccounting } from "@/types/AzAccounting";
 
 import productsData from "@/data/products.json";
@@ -18,7 +33,9 @@ import beersData from "@/data/beers.json";
 enum StepEnum {
   SELECT_PRODUCT = "SELECT_PRODUCT",
   SELECT_DRINK = "SELECT_DRINK",
-  SELECT_SODE = "SELECT_SODE",
+  SELECT_SODA = "SELECT_SODA",
+  SELECT_BEER = "SELECT_BEER",
+  SELECT_SHOT = "SELECT_SHOT",
   SELECT_PAYMENT = "SELECT_PAYMENT",
   ADD_PRODUCT = "ADD_PRODUCT",
 }
@@ -47,16 +64,70 @@ export default function Home() {
     }
   };
 
-  const handleProductTypeClick = (value: Product) => {
+  const handleBack = () => {
+    if (step === StepEnum.SELECT_DRINK) {
+      setStep(StepEnum.SELECT_PRODUCT);
+    }
+
+    if (
+      step === StepEnum.SELECT_SODA &&
+      (product?.type === SubTypeProduct.RUM ||
+        product?.type === SubTypeProduct.GIN ||
+        product?.type === SubTypeProduct.WHISKEY ||
+        product?.type === SubTypeProduct.VODKA)
+    ) {
+      setStep(StepEnum.SELECT_DRINK);
+    } else {
+      setStep(StepEnum.SELECT_PRODUCT);
+    }
+
+    if (step === StepEnum.SELECT_SHOT || step === StepEnum.SELECT_BEER) {
+      setStep(StepEnum.SELECT_PRODUCT);
+    }
+  };
+
+  const handleSetStepByProductType = (value: Product) => {
     setProduct(value);
+
+    if (value.type === ProductEnum.DRINK) {
+      setStep(StepEnum.SELECT_DRINK);
+    }
+
+    if (
+      value.type === SubTypeProduct.RUM ||
+      value.type === SubTypeProduct.GIN ||
+      value.type === SubTypeProduct.WHISKEY ||
+      value.type === SubTypeProduct.VODKA
+    ) {
+      setStep(StepEnum.SELECT_SODA);
+    }
+
+    if (value.type === ProductEnum.SODA) {
+      setStep(StepEnum.SELECT_SODA);
+    }
+
+    if (value.type === ProductEnum.BEER) {
+      setStep(StepEnum.SELECT_BEER);
+    }
+
+    if (value.type === ProductEnum.SHOT) {
+      setStep(StepEnum.SELECT_SHOT);
+    }
   };
 
   return (
     <div className="w-full h-full flex flex-col items-center justify-between gap-16">
-      <h1 className="text-4xl font-bold text-center">Selecciona producto</h1>
+      <header className="w-full grid grid-cols-3 items-center">
+        <div>
+          <ChevronLeft className="w-10 h-10" onClick={handleBack} />
+        </div>
+        <div>
+          <h1 className="text-4xl font-bold text-center">CopAZo</h1>
+        </div>
+      </header>
 
       <section className="w-full h-full">
-        {!product && (
+        {step === StepEnum.SELECT_PRODUCT && (
           <div className="grid grid-cols-2 gap-8 items-center justify-center">
             {productsData.map((product) => (
               <Card
@@ -64,67 +135,67 @@ export default function Home() {
                 title={product.name}
                 icon={renderIcon(product as Product)}
                 value={product as Product}
-                onClick={handleProductTypeClick}
+                onClick={handleSetStepByProductType}
               />
             ))}
           </div>
         )}
 
-        {product && product.type === ProductEnum.DRINK && (
+        {step === StepEnum.SELECT_DRINK && (
           <>
             <SectionProduct
               title="Rum"
-              products={drinksData.rum as Product[]}
-              onClick={handleProductTypeClick}
+              products={drinksData.rum as Drink[]}
+              onClick={handleSetStepByProductType}
               renderIcon={renderIcon}
             />
 
             <SectionProduct
               title="Gin"
-              products={drinksData.gin as Product[]}
-              onClick={handleProductTypeClick}
+              products={drinksData.gin as Drink[]}
+              onClick={handleSetStepByProductType}
               renderIcon={renderIcon}
             />
 
             <SectionProduct
               title="Whiskey"
-              products={drinksData.whiskey as Product[]}
-              onClick={handleProductTypeClick}
+              products={drinksData.whiskey as Drink[]}
+              onClick={handleSetStepByProductType}
               renderIcon={renderIcon}
             />
 
             <SectionProduct
               title="Vodka"
-              products={drinksData.vodka as Product[]}
-              onClick={handleProductTypeClick}
+              products={drinksData.vodka as Drink[]}
+              onClick={handleSetStepByProductType}
               renderIcon={renderIcon}
             />
           </>
         )}
 
-        {product && product.type === ProductEnum.SODA && (
+        {step === StepEnum.SELECT_SODA && (
           <SectionProduct
             title="Soda"
-            products={sodasData as Product[]}
-            onClick={handleProductTypeClick}
+            products={sodasData as Soda[]}
+            onClick={handleSetStepByProductType}
             renderIcon={renderIcon}
           />
         )}
 
-        {product && product.type === ProductEnum.BEER && (
+        {step === StepEnum.SELECT_BEER && (
           <SectionProduct
             title="Beer"
-            products={beersData as Product[]}
-            onClick={handleProductTypeClick}
+            products={beersData as Beer[]}
+            onClick={handleSetStepByProductType}
             renderIcon={renderIcon}
           />
         )}
 
-        {product && product.type === ProductEnum.SHOT && (
+        {step === StepEnum.SELECT_SHOT && (
           <SectionProduct
             title="Shot"
-            products={shotsData as Product[]}
-            onClick={handleProductTypeClick}
+            products={shotsData as Shot[]}
+            onClick={handleSetStepByProductType}
             renderIcon={renderIcon}
           />
         )}
