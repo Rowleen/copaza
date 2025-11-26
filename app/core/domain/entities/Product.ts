1615d8d6b654adf6ac0ca667d0a55f4b70fa66e0
Rowleen <julianpeacevedo@gmail.com>
2025-11-27 00:52:25 +0100

    Create subtype type

--- app/core/domain/entities/Product.ts
+++ app/core/domain/entities/Product.ts
@@ -5,11 +5,46 @@ export enum ProductEnum {
   BEER = "BEER",
 }
 
+export enum SubTypeProduct {
+  SODA = "SODA",
+  BEER = "BEER",
+  SHOT = "SHOT",
+  RUM = "RUM",
+  GIN = "GIN",
+  WHISKEY = "WHISKEY",
+  VODKA = "VODKA",
+  OTHER = "OTHER",
+}
+
 export interface Product {
   id: string;
   name: string;
-  type: ProductEnum;
+  type: ProductEnum | SubTypeProduct;
   price: number;
   image: string;
   description: string;
+}
+
+export interface Drink extends Product {
+  subType:
+    | SubTypeProduct.RUM
+    | SubTypeProduct.GIN
+    | SubTypeProduct.WHISKEY
+    | SubTypeProduct.VODKA;
+}
+
+export interface Shot extends Product {
+  subType: SubTypeProduct.SHOT;
+}
+
+export interface Soda extends Product {
+  subType: SubTypeProduct.SODA;
+}
+
+export interface Beer extends Product {
+  subType: SubTypeProduct.BEER;
+}
+
+export interface Other extends Product {
+  subType: SubTypeProduct.OTHER;
 }
