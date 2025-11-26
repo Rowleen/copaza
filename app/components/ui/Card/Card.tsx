bdf7d9aafe45ca920c1ece37865d39a4d05a606a
Rowleen <julianpeacevedo@gmail.com>
2025-11-26 20:37:44 +0100

    Implement onclick and add styles

--- app/components/ui/Card/Card.tsx
+++ app/components/ui/Card/Card.tsx
@@ -1,16 +1,20 @@
-import { FC, ReactElement } from "react";
+import { FC, ReactElement, MouseEvent } from "react";
+
+import { ProductEnum } from "@/entities/Product";
 
 interface CardProps {
   title: string;
   icon: ReactElement;
-  onClick: () => void;
+  dataProductType: ProductEnum;
+  onClick: (event: MouseEvent<HTMLDivElement>) => void;
 }
 
-const Card: FC<CardProps> = ({ title, icon, onClick }) => {
+const Card: FC<CardProps> = ({ title, icon, onClick, dataProductType }) => {
   return (
     <div
-      className="bg-zinc-800 rounded-lg p-4 flex items-center gap-2 cursor-pointer"
+      className="bg-zinc-800 w-full h-60 rounded-lg p-4 flex flex-col items-center justify-center gap-2 cursor-pointer hover:bg-zinc-700 transition-colors"
       onClick={onClick}
+      data-product-type={dataProductType}
     >
       {icon}
       <h2 className="text-2xl font-bold">{title}</h2>
