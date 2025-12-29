054c628c0baf6fba2a705c831dc2e1ffe4c19c99
Rowleen <julianpeacevedo@gmail.com>
2025-12-28 21:01:05 -0500

    Refactor component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,26 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import type { ItemType } from '@/types/items'
+import Card from '@/components/ui/Card'
+
+import consumption from '@/data/consumption.json'
+
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-slate-900 via-slate-800 to-slate-900"></div>
+    <section className="flex items-center justify-center h-[100vh]">
+      <div className="w-[800px] grid grid-cols-2 gap-6">
+        {consumption.map((item) => {
+          return (
+            <Card
+              key={item.id}
+              title={item.consumption}
+              type={item.type as unknown as ItemType}
+            />
+          )
+        })}
+      </div>
+    </section>
   )
 }
